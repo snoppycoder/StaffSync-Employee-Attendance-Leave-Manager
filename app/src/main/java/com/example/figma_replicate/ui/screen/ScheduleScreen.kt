cb21c9f1ad4bfff453b717cc41763a95239cc9ac
Sanji <snoppycoder>
2025-05-02 21:32:04 +0300

    WIP: local changes before merge

---
+++ app/src/main/java/com/example/figma_replicate/ui/screen/ScheduleScreen.kt
@@ -0,0 +1,21 @@
+package com.example.figma_replicate.ui.screen
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.figma_replicate.navigation.Routes
+import com.example.figma_replicate.ui.component.*
+@Composable
+@RequiresApi(Build.VERSION_CODES.O)
+fun ScheduleScreen() {
+
+}
