8d68735c598aa4e9a61d4b78bab9b9cd857fcf34
Sanji <snoppycoder>
2025-05-03 17:28:08 +0300

    Finished with profile Screen

--- app/src/main/java/com/example/figma_replicate/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/figma_replicate/ui/screen/ProfileScreen.kt
@@ -3,14 +3,20 @@ package com.example.figma_replicate.ui.component
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import com.example.figma_replicate.R
 
@@ -29,6 +36,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.figma_replicate.ui.theme.Typography
 
 @Composable
 fun ProfileScreen(navController: NavController){
@@ -36,8 +44,8 @@ fun ProfileScreen(navController: NavController){
 
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+            .fillMaxSize()
+            .padding(8.dp),
 
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -45,6 +53,7 @@ fun ProfileScreen(navController: NavController){
     Box(
         modifier = Modifier
             .size(80.dp)
+
             .clip(CircleShape)
     ) {
         Image(
@@ -77,72 +86,158 @@ fun ProfileScreen(navController: NavController){
 
 
         }
-        Column(){
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+            ,
+            verticalArrangement = Arrangement.spacedBy(3.dp)
+
+        ){
             Box() {
                 Button(
-                    onClick = { navController.navigate("edit_profile")},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(red = 255, green = 127, blue = 80)
-                    ), shape = RoundedCornerShape(8.dp),
+                    onClick = { navController.navigate("edit_profile") },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    shape = RoundedCornerShape(8.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 32.dp, vertical = 16.dp)
-
-
+                        .padding(vertical = 8.dp)
+                        .shadow(elevation = 8.dp, ambientColor = Color.Black)
                 ) {
-                    Text(text = "Edit Profile", fontWeight = FontWeight.Bold)
+
+                    Box(modifier = Modifier.fillMaxWidth()) {
+
+                        Icon(
+                            imageVector = Icons.Filled.Notifications,
+                            contentDescription = "Notification Setting",
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .padding(start = 16.dp)
+                                .size(24.dp)
+                        )
+
+
+                        Text(
+                            text = "Notification Settings",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            style = Typography.labelLarge,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
+
 
 
             }
             Box() {
                 Button(
-                    onClick = { navController.navigate("edit_profile")},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(red = 255, green = 127, blue = 80)
-                    ), shape = RoundedCornerShape(8.dp),
+                    onClick = { navController.navigate("edit_profile") },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    shape = RoundedCornerShape(8.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 32.dp, vertical = 16.dp)
-
-
+                        .padding(vertical = 8.dp)
+                        .shadow(elevation = 8.dp, ambientColor = Color.Black)
                 ) {
-                    Text(text = "Edit Profile", fontWeight = FontWeight.Bold)
+
+                    Box(modifier = Modifier.fillMaxWidth()) {
+
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_key),
+                            contentDescription = "Change Password",
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .padding(start = 8.dp)
+                                .size(24.dp)
+                        )
+
+
+                        Text(
+                            text = "Change Password",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            style = Typography.labelLarge,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
+
 
 
             }
             Box() {
                 Button(
-                    onClick = { navController.navigate("edit_profile")},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(red = 255, green = 127, blue = 80)
-                    ), shape = RoundedCornerShape(8.dp),
+                    onClick = { navController.navigate("edit_profile") },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    shape = RoundedCornerShape(8.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 32.dp, vertical = 16.dp)
-
-
+                        .padding(vertical = 8.dp)
+                        .shadow(elevation = 8.dp, ambientColor = Color.Black)
                 ) {
-                    Text(text = "Edit Profile", fontWeight = FontWeight.Bold)
+
+                    Box(modifier = Modifier.fillMaxWidth()) {
+
+                        Icon(
+                            imageVector = Icons.Filled.ExitToApp,
+                            contentDescription = "Logout",
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .padding(start = 16.dp)
+                                .size(24.dp)
+                        )
+
+
+                        Text(
+                            text = "Logout",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            style = Typography.labelLarge,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
+
 
 
             }
             Box() {
                 Button(
-                    onClick = { navController.navigate("edit_profile")},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(red = 255, green = 127, blue = 80)
-                    ), shape = RoundedCornerShape(8.dp),
+                    onClick = { navController.navigate("edit_profile") },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    shape = RoundedCornerShape(8.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 32.dp, vertical = 16.dp)
-
-
+                        .padding(vertical = 16.dp)
+                        .shadow(elevation = 8.dp, ambientColor = Color.Black)
                 ) {
-                    Text(text = "Edit Profile", fontWeight = FontWeight.Bold)
+
+                    Box(modifier = Modifier.fillMaxWidth()) {
+
+                        Icon(
+                            painter = painterResource(id=R.drawable.ic_delete),
+                            contentDescription = "Delete Account",
+                            tint = Color.hsl(5f, 1f, 0.73f),
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .padding(start = 16.dp)
+                                .size(24.dp)
+                        )
+
+
+                        Text(
+                            text = "Delete Account",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.hsl(5f, 1f, 0.73f),
+                            style = Typography.labelLarge,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
+
 
 
             }
