bbfcc0ec62f6b4d11fe14d3d3f5dc0afdd8b0390
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2025-05-06 19:55:49 +0300

    api-integration

--- app/src/main/java/com/example/figma_replicate/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/figma_replicate/ui/screen/HomeScreen.kt
@@ -72,7 +72,7 @@ fun HomeScreen() {
                 ScheduleScreen(navController)
             }
             composable(Routes.OFFICE) {
-                LeaveDashboardScreen(navController=navController)
+                UsersScreen()
                 // OfficeScreen()
             }
             composable(Routes.HOLIDAY) {
